9ef7d66accd78650298181740fd3537ee726ffe8
Olamen21 <134398136+Olamen21@users.noreply.github.com>
2025-06-16 10:48:03 +0700

    Revert "feature_maxlength"

--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './CustomInput.css';
 
-function CustomInput({ value, onChange, placeholder }) {
+function CustomInput({ value, onChange, placeholder, error }) {
   return (
-    <input 
-      type="text" 
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-    />
+    <div className="custom-input-container">
+      <input 
+        type="text" 
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={`custom-input ${error ? 'input-error' : ''}`}
+      />
+      {error && <div className="custom-input-error">{error}</div>}
+    </div>
   );
 }
 
